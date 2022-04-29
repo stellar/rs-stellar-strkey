e1426c33935f7186c59014b9727fce38836c0030
Tomer Weller <tomer.weller@gmail.com>
2022-04-28 21:04:05 -0700

    fixup

--- tests/private_key_ed25519.rs
+++ tests/private_key_ed25519.rs
@@ -3,23 +3,23 @@ extern crate proptest;
 use proptest::prelude::*;
 
 #[test]
-fn test_public_key_ed25519_from_string() {
+fn test_private_key_ed25519_from_string() {
 }
 
 proptest! {
     #[test]
-    fn test_public_key_ed25519_from_string_doesnt_panic(data: String) {
+    fn test_private_key_ed25519_from_string_doesnt_panic(data: String) {
         let _ = Strkey::from_string(&data);
     }
 }
 
 #[test]
-fn test_public_key_ed25519_to_string() {
+fn test_private_key_ed25519_to_string() {
 }
 
 proptest! {
     #[test]
-    fn test_public_key_ed25519_to_string_doesnt_panic(data: [u8; 32]) {
+    fn test_private_key_ed25519_to_string_doesnt_panic(data: [u8; 32]) {
         Strkey::PrivateKeyEd25519(data).to_string();
     }
 }

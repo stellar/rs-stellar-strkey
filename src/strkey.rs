18c631ebf4f52cb4d6cc791b262c682051ab3b56
Jun Luo <4catcode@gmail.com>
2022-06-22 10:09:10 +0800

    add HashTx, HashX, MuxedAccountEd25519 and SignedPayloadEd25519 support.

--- src/strkey.rs
+++ src/strkey.rs
@@ -10,6 +10,10 @@ pub enum DecodeError {
 pub enum Strkey {
     PublicKeyEd25519(StrkeyPublicKeyEd25519),
     PrivateKeyEd25519(StrkeyPrivateKeyEd25519),
+    HashTx(StrkeyHashTx),
+    HashX(StrkeyHashX),
+    MuxedAccountEd25519(StrkeyMuxedAccountEd25519),
+    SignedPayloadEd25519(StrkeySignedPayloadEd25519),
 }
 
 impl Strkey {
@@ -17,6 +21,10 @@ impl Strkey {
         match self {
             Self::PublicKeyEd25519(x) => x.to_string(),
             Self::PrivateKeyEd25519(x) => x.to_string(),
+            Self::HashTx(x) => x.to_string(),
+            Self::HashX(x) => x.to_string(),
+            Self::MuxedAccountEd25519(x) => x.to_string(),
+            Self::SignedPayloadEd25519(x) => x.to_string(),
         }
     }
 
@@ -28,6 +36,14 @@ impl Strkey {
             )),
             version::PRIVATE_KEY_ED25519 => Ok(Self::PrivateKeyEd25519(
                 StrkeyPrivateKeyEd25519::from_payload(&payload)?,
+            )),
+            version::HASH_TX => Ok(Self::HashTx(StrkeyHashTx::from_payload(&payload)?)),
+            version::HASH_X => Ok(Self::HashX(StrkeyHashX::from_payload(&payload)?)),
+            version::MUXED_ACCOUNT_ED25519 => Ok(Self::MuxedAccountEd25519(
+                StrkeyMuxedAccountEd25519::from_payload(&payload)?,
+            )),
+            version::SIGNED_PAYLOAD_ED25519 => Ok(Self::SignedPayloadEd25519(
+                StrkeySignedPayloadEd25519::from_payload(&payload)?,
             )),
             _ => Err(DecodeError::Invalid),
         }
@@ -82,17 +98,134 @@ impl StrkeyPrivateKeyEd25519 {
     }
 }
 
+#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
+pub struct StrkeyMuxedAccountEd25519(pub [u8; 40]);
+
+impl StrkeyMuxedAccountEd25519 {
+    pub fn to_string(&self) -> String {
+        encode(version::MUXED_ACCOUNT_ED25519, &self.0)
+    }
+
+    fn from_payload(payload: &[u8]) -> Result<Self, DecodeError> {
+        match payload.try_into() {
+            Ok(muxed) => Ok(Self(muxed)),
+            Err(_) => Err(DecodeError::Invalid),
+        }
+    }
+
+    pub fn from_string(s: &str) -> Result<Self, DecodeError> {
+        let (ver, payload) = decode(s)?;
+        match ver {
+            version::MUXED_ACCOUNT_ED25519 => Self::from_payload(&payload),
+            _ => Err(DecodeError::Invalid),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
+pub struct StrkeyHashTx(pub [u8; 32]);
+
+impl StrkeyHashTx {
+    pub fn to_string(&self) -> String {
+        encode(version::HASH_TX, &self.0)
+    }
+
+    fn from_payload(payload: &[u8]) -> Result<Self, DecodeError> {
+        match payload.try_into() {
+            Ok(hash_tx) => Ok(Self(hash_tx)),
+            Err(_) => Err(DecodeError::Invalid),
+        }
+    }
+
+    pub fn from_string(s: &str) -> Result<Self, DecodeError> {
+        let (ver, payload) = decode(s)?;
+        match ver {
+            version::HASH_TX => Self::from_payload(&payload),
+            _ => Err(DecodeError::Invalid),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
+pub struct StrkeyHashX(pub [u8; 32]);
+
+impl StrkeyHashX {
+    pub fn to_string(&self) -> String {
+        encode(version::HASH_X, &self.0)
+    }
+
+    fn from_payload(payload: &[u8]) -> Result<Self, DecodeError> {
+        match payload.try_into() {
+            Ok(hash_x) => Ok(Self(hash_x)),
+            Err(_) => Err(DecodeError::Invalid),
+        }
+    }
+
+    pub fn from_string(s: &str) -> Result<Self, DecodeError> {
+        let (ver, payload) = decode(s)?;
+        match ver {
+            version::HASH_X => Self::from_payload(&payload),
+            _ => Err(DecodeError::Invalid),
+        }
+    }
+}
+
+#[derive(Clone, Hash, PartialEq, Eq, Debug)]
+// The largest strkey is a signed payload:
+// 32-byte public key + 4-byte payload length + 64-byte payload
+pub struct StrkeySignedPayloadEd25519(pub Vec<u8>);
+
+impl StrkeySignedPayloadEd25519 {
+    pub fn to_string(&self) -> String {
+        encode(version::SIGNED_PAYLOAD_ED25519, &self.0)
+    }
+
+    fn from_payload(payload: &[u8]) -> Result<Self, DecodeError> {
+        match payload.try_into() {
+            Ok(signed_payload) => {
+                let payload_len = payload.len() as u32;
+                if payload_len < 32 + 4 + 4 || payload_len > 32 + 4 + 64 {
+                    return Err(DecodeError::Invalid);
+                }
+                let inner_payload_len =
+                    u32::from_be_bytes((&payload[32..32 + 4]).try_into().unwrap());
+                if inner_payload_len + ((4 - inner_payload_len % 4) % 4) != payload_len - 32 - 4 {
+                    return Err(DecodeError::Invalid);
+                }
+                Ok(Self(signed_payload))
+            }
+            Err(_) => Err(DecodeError::Invalid),
+        }
+    }
+
+    pub fn from_string(s: &str) -> Result<Self, DecodeError> {
+        let (ver, payload) = decode(s)?;
+        match ver {
+            version::SIGNED_PAYLOAD_ED25519 => Self::from_payload(&payload),
+            _ => Err(DecodeError::Invalid),
+        }
+    }
+}
+
 mod version {
     use super::public_key_alg::*;
-    use super::typ::*;
+    use super::typ;
 
-    pub const PUBLIC_KEY_ED25519: u8 = PUBLIC_KEY | ED25519;
-    pub const PRIVATE_KEY_ED25519: u8 = PRIVATE_KEY | ED25519;
+    pub const PUBLIC_KEY_ED25519: u8 = typ::PUBLIC_KEY | ED25519;
+    pub const PRIVATE_KEY_ED25519: u8 = typ::PRIVATE_KEY | ED25519;
+    pub const MUXED_ACCOUNT_ED25519: u8 = typ::MUXED_ACCOUNT | ED25519;
+    pub const HASH_TX: u8 = typ::HASH_TX;
+    pub const HASH_X: u8 = typ::HASH_X;
+    pub const SIGNED_PAYLOAD_ED25519: u8 = typ::SIGNED_PAYLOAD | ED25519;
 }
 
 mod typ {
     pub const PUBLIC_KEY: u8 = 6 << 3;
     pub const PRIVATE_KEY: u8 = 18 << 3;
+    pub const MUXED_ACCOUNT: u8 = 12 << 3;
+    pub const HASH_TX: u8 = 19 << 3;
+    pub const HASH_X: u8 = 23 << 3;
+    pub const SIGNED_PAYLOAD: u8 = 15 << 3;
 }
 
 mod public_key_alg {
--- tests/tests.rs
+++ tests/tests.rs
@@ -47,6 +47,134 @@ fn test_valid_private_keys() {
     );
 }
 
+#[test]
+fn test_valid_hash_tx() {
+    assert_convert_roundtrip(
+        "TA3D5KRYM6CB7OWQ6TWYRR3Z4T7GNZLKERYNZGGA5SOAOPIFY6YQGZAM",
+        &Strkey::HashTx(StrkeyHashTx([
+            0x36, 0x3e, 0xaa, 0x38, 0x67, 0x84, 0x1f, 0xba, 0xd0, 0xf4, 0xed, 0x88, 0xc7, 0x79,
+            0xe4, 0xfe, 0x66, 0xe5, 0x6a, 0x24, 0x70, 0xdc, 0x98, 0xc0, 0xec, 0x9c, 0x07, 0x3d,
+            0x05, 0xc7, 0xb1, 0x03,
+        ])),
+    );
+}
+
+#[test]
+fn test_valid_hash_x() {
+    assert_convert_roundtrip(
+        "XA3D5KRYM6CB7OWQ6TWYRR3Z4T7GNZLKERYNZGGA5SOAOPIFY6YQH5FV",
+        &Strkey::HashX(StrkeyHashX([
+            0x36, 0x3e, 0xaa, 0x38, 0x67, 0x84, 0x1f, 0xba, 0xd0, 0xf4, 0xed, 0x88, 0xc7, 0x79,
+            0xe4, 0xfe, 0x66, 0xe5, 0x6a, 0x24, 0x70, 0xdc, 0x98, 0xc0, 0xec, 0x9c, 0x07, 0x3d,
+            0x05, 0xc7, 0xb1, 0x03,
+        ])),
+    );
+}
+
+#[test]
+fn test_valid_muxed_ed25519() {
+    // Valid multiplexed account (id: 123456)
+    assert_convert_roundtrip(
+        "MA3D5KRYM6CB7OWQ6TWYRR3Z4T7GNZLKERYNZGGA5SOAOPIFY6YQGAAAAAAAAAPCICBKU",
+        &Strkey::MuxedAccountEd25519(StrkeyMuxedAccountEd25519([
+            0x36, 0x3e, 0xaa, 0x38, 0x67, 0x84, 0x1f, 0xba, 0xd0, 0xf4, 0xed, 0x88, 0xc7, 0x79,
+            0xe4, 0xfe, 0x66, 0xe5, 0x6a, 0x24, 0x70, 0xdc, 0x98, 0xc0, 0xec, 0x9c, 0x07, 0x3d,
+            0x05, 0xc7, 0xb1, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0xe2, 0x40,
+        ])),
+    );
+
+    // Valid multiplexed account (id: 0)
+    assert_convert_roundtrip(
+        "MA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAAAAAAAACJUQ",
+        &Strkey::MuxedAccountEd25519(StrkeyMuxedAccountEd25519([
+            0x3f, 0x0c, 0x34, 0xbf, 0x93, 0xad, 0x0d, 0x99, 0x71, 0xd0, 0x4c, 0xcc, 0x90, 0xf7,
+            0x05, 0x51, 0x1c, 0x83, 0x8a, 0xad, 0x97, 0x34, 0xa4, 0xa2, 0xfb, 0x0d, 0x7a, 0x03,
+            0xfc, 0x7f, 0xe8, 0x9a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ])),
+    );
+
+    // Valid multiplexed account in which unsigned id exceeds maximum signed 64-bit integer
+    assert_convert_roundtrip(
+        "MA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJVAAAAAAAAAAAAAJLK",
+        &Strkey::MuxedAccountEd25519(StrkeyMuxedAccountEd25519([
+            0x3f, 0x0c, 0x34, 0xbf, 0x93, 0xad, 0x0d, 0x99, 0x71, 0xd0, 0x4c, 0xcc, 0x90, 0xf7,
+            0x05, 0x51, 0x1c, 0x83, 0x8a, 0xad, 0x97, 0x34, 0xa4, 0xa2, 0xfb, 0x0d, 0x7a, 0x03,
+            0xfc, 0x7f, 0xe8, 0x9a, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ])),
+    );
+}
+
+#[test]
+fn test_invalid_muxed_ed25519() {
+    // TODO: This test case is supposed to fail, but it will pass, I think this is the responsibility of the base32 lib
+    // maybe related to https://github.com/stellar/rs-stellar-strkey/issues/10
+    // The unused trailing bit must be zero in the encoding of the last three bytes (24 bits) as five base-32 symbols (25 bits)
+    // let r = Strkey::from_string("MA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAAAAAAAACJUR");
+    // assert_eq!(r, Err(DecodeError::Invalid));
+
+    // Invalid length (congruent to 6 mod 8)
+    let r = Strkey::from_string(
+        "MA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJVAAAAAAAAAAAAAJLKA",
+    );
+    assert_eq!(r, Err(DecodeError::Invalid));
+
+    // Invalid length (base-32 decoding should yield 43 bytes, not 44)
+    let r = Strkey::from_string(
+        "MA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJVAAAAAAAAAAAAAAV75I",
+    );
+    assert_eq!(r, Err(DecodeError::Invalid));
+
+    // Invalid algorithm (low 3 bits of version byte are 7)
+    let r = Strkey::from_string(
+        "M47QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAAAAAAAACJUQ",
+    );
+    assert_eq!(r, Err(DecodeError::Invalid));
+
+    // Padding bytes are not allowed
+    let r = Strkey::from_string(
+        "MA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAAAAAAAACJUK===",
+    );
+    assert_eq!(r, Err(DecodeError::Invalid));
+
+    // Invalid checksum
+    let r = Strkey::from_string(
+        "MA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAAAAAAAACJUO",
+    );
+    assert_eq!(r, Err(DecodeError::Invalid));
+}
+
+#[test]
+fn test_valid_signed_payload_ed25519() {
+    assert_convert_roundtrip(
+        "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAQACAQDAQCQMBYIBEFAWDANBYHRAEISCMKBKFQXDAMRUGY4DUPB6IBZGM",
+        &Strkey::SignedPayloadEd25519(StrkeySignedPayloadEd25519(vec![
+            0x3f, 0xc, 0x34, 0xbf, 0x93, 0xad, 0xd, 0x99, 0x71, 0xd0, 0x4c, 0xcc, 0x90, 0xf7, 0x5, 0x51, 0x1c, 0x83, 0x8a, 0xad, 0x97, 0x34, 0xa4, 0xa2, 0xfb, 0xd, 0x7a, 0x3, 0xfc, 0x7f, 0xe8, 0x9a, 0x0, 0x0, 0x0, 0x20, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20,
+        ])),
+    );
+
+    assert_convert_roundtrip(
+        "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAOQCAQDAQCQMBYIBEFAWDANBYHRAEISCMKBKFQXDAMRUGY4DUAAAAFGBU",
+        &Strkey::SignedPayloadEd25519(StrkeySignedPayloadEd25519(vec![
+            0x3f, 0xc, 0x34, 0xbf, 0x93, 0xad, 0xd, 0x99, 0x71, 0xd0, 0x4c, 0xcc, 0x90, 0xf7, 0x5, 0x51, 0x1c, 0x83, 0x8a, 0xad, 0x97, 0x34, 0xa4, 0xa2, 0xfb, 0xd, 0x7a, 0x3, 0xfc, 0x7f, 0xe8, 0x9a, 0x0, 0x0, 0x0, 0x1d, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x0, 0x0, 0x0,
+        ])),
+    );
+}
+
+#[test]
+fn test_invalid_signed_payload_ed25519() {
+    // Length prefix specifies length that is shorter than payload in signed payload
+    let r = Strkey::from_string("PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAQACAQDAQCQMBYIBEFAWDANBYHRAEISCMKBKFQXDAMRUGY4DUPB6IAAAAAAAAPM");
+    assert_eq!(r, Err(DecodeError::Invalid));
+
+    // Length prefix specifies length that is longer than payload in signed payload
+    let r = Strkey::from_string("PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAOQCAQDAQCQMBYIBEFAWDANBYHRAEISCMKBKFQXDAMRUGY4Z2PQ");
+    assert_eq!(r, Err(DecodeError::Invalid));
+
+    // No zero padding in signed payload
+    let r = Strkey::from_string("PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAOQCAQDAQCQMBYIBEFAWDANBYHRAEISCMKBKFQXDAMRUGY4DXFH6");
+    assert_eq!(r, Err(DecodeError::Invalid));
+}
+
 proptest! {
     #[test]
     fn test_public_key_ed25519_from_string_doesnt_panic(data: String) {
@@ -65,5 +193,5 @@ fn assert_convert_roundtrip(s: &str, strkey: &Strkey) {
     let strkey_result = Strkey::from_string(&s).unwrap();
     assert_eq!(&strkey_result, strkey);
     let str_result = strkey.to_string();
-    assert_eq!(str_result, str_result)
+    assert_eq!(str_result, s)
 }

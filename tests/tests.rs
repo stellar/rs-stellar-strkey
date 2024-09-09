79a3faee92413e5d41bd0172144006711204bce0
Jun Luo <4catcode@gmail.com>
2024-09-09 08:50:38 +0800

    Revert "WIP, no alloc"
    This reverts commit 722ee54869e2c7661b937de15dcd5580c7ab6c25.

--- tests/tests.rs
+++ tests/tests.rs
@@ -179,85 +179,85 @@ fn test_valid_signed_payload_ed25519() {
             payload_len: 32,
         }),
     );
-
-    assert_convert_roundtrip(
-        "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAOQCAQDAQCQMBYIBEFAWDANBYHRAEISCMKBKFQXDAMRUGY4DUAAAAFGBU",
-        &Strkey::SignedPayloadEd25519(ed25519::SignedPayload {
-            ed25519: [0x3f, 0xc, 0x34, 0xbf, 0x93, 0xad, 0xd, 0x99, 0x71, 0xd0, 0x4c, 0xcc, 0x90, 0xf7, 0x5, 0x51, 0x1c, 0x83, 0x8a, 0xad, 0x97, 0x34, 0xa4, 0xa2, 0xfb, 0xd, 0x7a, 0x3, 0xfc, 0x7f, 0xe8, 0x9a, ],
-            payload: [
-                0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d,
-                0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-            ],
-            payload_len: 29,
-        }),
-    );
-
-    // Unused trailing bits are zero
-    // A signed payload with a 1 byte version, 32 byte key, and 4 byte length,
-    // has the number of total bytes, total bits, and tail bits depending on its payload length:
     //
-    // |Version|Key|Length|Payload|CRC|Total|Bits|Unused|
-    // |------:|--:|-----:|------:|--:|----:|---:|-----:|
-    // |      1| 32|     4|     16|  2|   55| 440|     0|
-    // |      1| 32|     4|      4|  2|   43| 344|     1|
-    // |      1| 32|     4|     12|  2|   51| 408|     2|
-    // |      1| 32|     4|     20|  2|   59| 472|     3|
-    // |      1| 32|     4|      8|  2|   47| 376|     4|
+    // assert_convert_roundtrip(
+    //     "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAOQCAQDAQCQMBYIBEFAWDANBYHRAEISCMKBKFQXDAMRUGY4DUAAAAFGBU",
+    //     &Strkey::SignedPayloadEd25519(ed25519::SignedPayload {
+    //         ed25519: [0x3f, 0xc, 0x34, 0xbf, 0x93, 0xad, 0xd, 0x99, 0x71, 0xd0, 0x4c, 0xcc, 0x90, 0xf7, 0x5, 0x51, 0x1c, 0x83, 0x8a, 0xad, 0x97, 0x34, 0xa4, 0xa2, 0xfb, 0xd, 0x7a, 0x3, 0xfc, 0x7f, 0xe8, 0x9a, ],
+    //         payload: [
+    //             0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d,
+    //             0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    //         ],
+    //         payload_len: 29,
+    //     }),
+    // );
     //
-    // Where:
-    // - Unused bits are calculated as (5 - Bits % 5) % 5
-    //
-    // Examples using key:
-    let ed25519 = [
-        0x3f, 0xc, 0x34, 0xbf, 0x93, 0xad, 0xd, 0x99, 0x71, 0xd0, 0x4c, 0xcc, 0x90, 0xf7, 0x5,
-        0x51, 0x1c, 0x83, 0x8a, 0xad, 0x97, 0x34, 0xa4, 0xa2, 0xfb, 0xd, 0x7a, 0x3, 0xfc, 0x7f,
-        0xe8, 0x9a,
-    ];
-    // - 0 unused bits:
-    assert_convert_roundtrip(
-        "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAIAAAAAAAAAAAAAAAAAAAAAAAAAAKB5",
-        &Strkey::SignedPayloadEd25519(ed25519::SignedPayload {
-            ed25519,
-            payload: [0; 64],
-            payload_len: 16,
-        }),
-    );
-    // - 1 unused bits:
-    assert_convert_roundtrip(
-        "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAACAAAAAABNWS",
-        &Strkey::SignedPayloadEd25519(ed25519::SignedPayload {
-            ed25519,
-            payload: [0; 64],
-            payload_len: 4,
-        }),
-    );
-    // - 2 unused bits:
-    assert_convert_roundtrip(
-        "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAGAAAAAAAAAAAAAAAAAAACTPY",
-        &Strkey::SignedPayloadEd25519(ed25519::SignedPayload {
-            ed25519,
-            payload: [0; 64],
-            payload_len: 12,
-        }),
-    );
-    // - 3 unused bits:
-    assert_convert_roundtrip(
-        "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAKAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALGXI",
-        &Strkey::SignedPayloadEd25519(ed25519::SignedPayload {
-            ed25519,
-            payload: [0; 64],
-            payload_len: 20,
-        }),
-    );
-    // - 4 unused bits:
-    assert_convert_roundtrip(
-        "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAEAAAAAAAAAAAAARKYQ",
-        &Strkey::SignedPayloadEd25519(ed25519::SignedPayload {
-            ed25519,
-            payload: [0; 64],
-            payload_len: 8,
-        }),
-    );
+    // // Unused trailing bits are zero
+    // // A signed payload with a 1 byte version, 32 byte key, and 4 byte length,
+    // // has the number of total bytes, total bits, and tail bits depending on its payload length:
+    // //
+    // // |Version|Key|Length|Payload|CRC|Total|Bits|Unused|
+    // // |------:|--:|-----:|------:|--:|----:|---:|-----:|
+    // // |      1| 32|     4|     16|  2|   55| 440|     0|
+    // // |      1| 32|     4|      4|  2|   43| 344|     1|
+    // // |      1| 32|     4|     12|  2|   51| 408|     2|
+    // // |      1| 32|     4|     20|  2|   59| 472|     3|
+    // // |      1| 32|     4|      8|  2|   47| 376|     4|
+    // //
+    // // Where:
+    // // - Unused bits are calculated as (5 - Bits % 5) % 5
+    // //
+    // // Examples using key:
+    // let ed25519 = [
+    //     0x3f, 0xc, 0x34, 0xbf, 0x93, 0xad, 0xd, 0x99, 0x71, 0xd0, 0x4c, 0xcc, 0x90, 0xf7, 0x5,
+    //     0x51, 0x1c, 0x83, 0x8a, 0xad, 0x97, 0x34, 0xa4, 0xa2, 0xfb, 0xd, 0x7a, 0x3, 0xfc, 0x7f,
+    //     0xe8, 0x9a,
+    // ];
+    // // - 0 unused bits:
+    // assert_convert_roundtrip(
+    //     "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAIAAAAAAAAAAAAAAAAAAAAAAAAAAKB5",
+    //     &Strkey::SignedPayloadEd25519(ed25519::SignedPayload {
+    //         ed25519,
+    //         payload: [0; 64],
+    //         payload_len: 16,
+    //     }),
+    // );
+    // // - 1 unused bits:
+    // assert_convert_roundtrip(
+    //     "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAACAAAAAABNWS",
+    //     &Strkey::SignedPayloadEd25519(ed25519::SignedPayload {
+    //         ed25519,
+    //         payload: [0; 64],
+    //         payload_len: 4,
+    //     }),
+    // );
+    // // - 2 unused bits:
+    // assert_convert_roundtrip(
+    //     "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAGAAAAAAAAAAAAAAAAAAACTPY",
+    //     &Strkey::SignedPayloadEd25519(ed25519::SignedPayload {
+    //         ed25519,
+    //         payload: [0; 64],
+    //         payload_len: 12,
+    //     }),
+    // );
+    // // - 3 unused bits:
+    // assert_convert_roundtrip(
+    //     "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAKAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALGXI",
+    //     &Strkey::SignedPayloadEd25519(ed25519::SignedPayload {
+    //         ed25519,
+    //         payload: [0; 64],
+    //         payload_len: 20,
+    //     }),
+    // );
+    // // - 4 unused bits:
+    // assert_convert_roundtrip(
+    //     "PA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJUAAAAAEAAAAAAAAAAAAARKYQ",
+    //     &Strkey::SignedPayloadEd25519(ed25519::SignedPayload {
+    //         ed25519,
+    //         payload: [0; 64],
+    //         payload_len: 8,
+    //     }),
+    // );
 }
 
 #[test]
